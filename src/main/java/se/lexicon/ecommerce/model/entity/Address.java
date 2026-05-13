31520ec7449819fa78895de449e3d3de7eef8965
Maja <maja.bloom@outlook.com>
2026-05-13 15:38:16 +0200

    add fields in Address

--- src/main/java/se/lexicon/ecommerce/model/entity/Address.java
+++ src/main/java/se/lexicon/ecommerce/model/entity/Address.java
@@ -1,1 +1,30 @@
-package se.lexicon.ecommerce.model;
+package se.lexicon.ecommerce.model.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// Mark this class as DB managed object
+@Entity
+// Define specific name of DB table
+@Table(name = "addresses")
+// Lombok: Automatically generate getters, setters, toString etc
+@Data
+// Lombok: Create constructor with no arguments
+@NoArgsConstructor
+// Lombok: Create a constructor with all fields as arguments
+@AllArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String street;
+    private String city;
+    private String zipCode;
+
+    // Fields will go here
+
+
+}
