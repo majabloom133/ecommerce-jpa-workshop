c72b88993ec029e55930121b22509e365d5dc288
Maja <maja.bloom@outlook.com>
2026-05-13 16:27:17 +0200

    feat: add Customer entity w. basic fields

--- src/main/java/se/lexicon/ecommerce/model/entity/Customer.java
+++ src/main/java/se/lexicon/ecommerce/model/entity/Customer.java
@@ -1,4 +1,28 @@
 package se.lexicon.ecommerce.model.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // Unique ID for each customer
+    private Long id;
+
+    // Nullable means an email must be entered.
+    // @Column makes sure noone has the same email address
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    // The date the user joined
+    private LocalDate registrationDate;
+
+
+
+
 }
