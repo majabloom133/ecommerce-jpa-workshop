731d3dde35ce179efcc214ea977ab416a978934f
Maja <maja.bloom@outlook.com>
2026-05-15 10:01:40 +0200

    refactor: update UserProfile fields and constructor feat: add bidirectional relation to UserProfile using mappedBy

--- src/main/java/se/lexicon/ecommerce/model/entity/UserProfile.java
+++ src/main/java/se/lexicon/ecommerce/model/entity/UserProfile.java
@@ -18,6 +18,26 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserProfile {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Nickname is mandatory and max 100 characters
+    @Column(nullable = false, length = 100)
+    private String nickname;
+
+    // Phone number is mandatory and max 100 characters
+    @Column(nullable = false, name = "phone_number", length = 100)
+    private String phoneNumber;
+
+    // Bio is optional, but max 500 characters
+    @Column(length = 500)
+    private String bio;
+
+    // Link back to Customer (Inverse side of bidirectional relationship)
+    @OneToOne(mappedBy = "userProfile")
+    private Customer customer;
+
     // I'll add fields here in next step
 
 }
