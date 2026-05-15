8666e47a44fdd0e42eaeb50cbb9307b1d5cebf14
Maja <maja.bloom@outlook.com>
2026-05-15 09:29:29 +0200

    feat: create UserProfile entity structure and annotations

--- src/main/java/se/lexicon/ecommerce/model/entity/UserProfile.java
+++ src/main/java/se/lexicon/ecommerce/model/entity/UserProfile.java
@@ -1,4 +1,23 @@
 package se.lexicon.ecommerce.model.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+// Mark this class as a  database entity
+@Entity
+// Name table for storing profile info
+@Table(name = "user_profiles")
+// Use Lombok to handle boilerplate code like getters and setters
+@Data
+// JPA requires a no-args constructor
+@NoArgsConstructor
+// Convinient constructor for initializing all fields
+@AllArgsConstructor
 public class UserProfile {
+
+    // I'll add fields here in next step
+
 }
