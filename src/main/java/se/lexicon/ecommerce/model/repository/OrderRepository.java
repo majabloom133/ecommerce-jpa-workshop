d7e621bd9692661b34abddf51ccfcd5f124f3560
Maja <maja.bloom@outlook.com>
2026-06-01 12:00:39 +0200

    Create OrderRepository interface extending JpaRepository

---
+++ src/main/java/se/lexicon/ecommerce/model/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package se.lexicon.ecommerce.model.repository;
+
+import se.lexicon.ecommerce.model.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
