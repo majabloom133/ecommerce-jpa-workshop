512788f54eb7c227661107c2fc4f48e5ae80eb18
Maja <maja.bloom@outlook.com>
2026-06-01 10:57:18 +0200

    Create OrderItem entity w. basic fields id, quantity and priceAtPurchase

---
+++ src/main/java/se/lexicon/ecommerce/model/entity/OrderItem.java
@@ -0,0 +1,48 @@
+package se.lexicon.ecommerce.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "order_items")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer quantity;
+
+    @Column(name = "price_at_purchase")
+    private BigDecimal priceAtPurchase;
+
+    // --- Getters & Setters ---
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPriceAtPurchase() {
+        return priceAtPurchase;
+    }
+
+    public void setPriceAtPurchase(BigDecimal priceAtPurchase) {
+        this.priceAtPurchase = priceAtPurchase;
+    }
+}
