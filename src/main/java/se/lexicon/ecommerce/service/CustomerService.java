7cc668be7266ca986b131a25c1e394783e4fdba9
Maja <maja.bloom@outlook.com>
2026-06-04 11:37:14 +0200

    feat: define CustomerService interface

---
+++ src/main/java/se/lexicon/ecommerce/service/CustomerService.java
@@ -0,0 +1,15 @@
+package se.lexicon.ecommerce.service;
+
+import se.lexicon.ecommerce.dto.CustomerRequest;
+import se.lexicon.ecommerce.dto.CustomerResponse;
+
+public interface CustomerService {
+
+    // Register new customer in system using provided request data
+    CustomerResponse register(CustomerRequest request);
+
+    // Find and return a customer by their id and new request data
+    CustomerResponse update(Long id, CustomerRequest request);
+}
+
+
