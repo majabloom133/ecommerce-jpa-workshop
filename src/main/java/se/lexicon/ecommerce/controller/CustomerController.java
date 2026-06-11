8403e9eda7cea56dd8cab229693d4679660e9c22
Maja <maja.bloom@outlook.com>
2026-06-11 08:57:13 +0200

    feat: setup CustomerController w. dependency injection

--- src/main/java/se/lexicon/ecommerce/controller/CustomerController.java
+++ src/main/java/se/lexicon/ecommerce/controller/CustomerController.java
@@ -1,4 +1,24 @@
 package se.lexicon.ecommerce.controller;
 
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import se.lexicon.ecommerce.dto.CustomerRequest;
+import se.lexicon.ecommerce.dto.CategoryResponse;
+import se.lexicon.ecommerce.service.CustomerService;
+
+@RestController
+@RequestMapping("/api/v1/customers")
 public class CustomerController {
+
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+
+
+
 }
